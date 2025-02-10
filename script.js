547d508cad1ad1d5983367d57c8190e161e0bfc1
Waqas <wmahmo3079@gmail.com>
2025-02-10 09:53:25 -0500

    initial commit

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
+    "진짜?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
+    "Princess please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "JK YOUR MINE AND YOU CANT SAY NO ❤️"
 ];
 
 let messageIndex = 0;
